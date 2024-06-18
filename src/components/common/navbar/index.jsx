6d7740bc94ffc8bdc4ad1b528fad5704d5a49599
Airyyy23 <wayangseno123@gmail.com>
2024-06-18 11:19:33 +0700

    feat [WS] create navbar

---
+++ src/components/common/navbar/index.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import navigationItems from "../../utils/navItems";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="2xl:px-44 xl:px-36 lg:px-28 md:px-20 xs:px-12 w-screen flex justify-between items-center py-6">
+      <h1 className="font-bold text-3xl">Logo</h1>
+
+      <div className={`md:static absolute duration-500 ease-in-out bg-[#f5f5f5] md:min-h-fit min-h-[40vh] w-full md:w-auto top-[9%] flex items-center justify-center px-5 ${isOpen? "left-0" : "left-[-100%]"}`}>
+        <ul className="font-medium flex md:gap-4 gap-8 md:flex-row flex-col items-center">
+          {navigationItems.map((item) => (
+            <li key={item.name}>
+              <Link to={item.link}>
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <button onClick={toggleMenu} className="md:hidden">
+        <i className={`fas ${isOpen? "fa-times" : "fa-bars"}`}></i>
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/utils/navItems.js
@@ -0,0 +1,9 @@
+const navigationItems = [
+   { name: "Beranda", link: "/" },
+   { name: "Produk", link: "/produk" },
+   { name: "Artikel", link: "/artikel" },
+   { name: "Tentang Kami", link: "/tentang-kami" },
+   { name: "Kontak", link: "/kontak" },
+];
+
+export default navigationItems;
