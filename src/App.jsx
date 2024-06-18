1ddad3cf3ba8b898ee9da040edfc33bb9ff8d88f
Airyyy23 <wayangseno123@gmail.com>
2024-06-18 11:04:55 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/common/navbar';
+import Footer from './components/common/footer';
+import Homepage from './pages/home';
+import Product from './pages/product';
+import NotFound from './pages/error/notFound';
+
+function App() {
+  return (
+    <Router>
+      <div className="h-screen relative flex flex-col justify-between items-center">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
